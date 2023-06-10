5985c41e50ff570e00240f38c1b2f290de3ea2e4
Tarık Yüzgül <yuzgultarik@gmail.com>
2023-06-11 00:50:15 +0300

    EntityLayer Eklendi.

---
+++ Controllers/KitapController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KutuphaneApp.Controllers
+{
+    public class KitapController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
