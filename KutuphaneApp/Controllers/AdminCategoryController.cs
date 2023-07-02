7093a077344002a3ca3cc1e4c5e9988efed19437
Tarık Yüzgül <yuzgultarik@gmail.com>
2023-07-02 15:00:14 +0300

    dist ve plugins klasorleri wwwroot altına taşındı. _AdminLayout düzenlendi türkçeleştirildi.

--- KutuphaneApp/Controllers/AdminCategoryController.cs
+++ KutuphaneApp/Controllers/AdminCategoryController.cs
@@ -1,12 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using DataAccsessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KutuphaneApp.Controllers
 {
     public class AdminCategoryController : Controller
     {
+
+        private readonly ICategoryDal _categoryDal;
+
+        public AdminCategoryController(ICategoryDal categoryDal)
+        {
+            _categoryDal = categoryDal;
+        }
+
+        //CategoryManager cm = new CategoryManager(new EfCategoryDal()); Olması gereken
         public IActionResult Index()
         {
-            return View();
+            var categoryValues = _categoryDal.List();
+            return View(categoryValues);
         }
 
         public IActionResult _AdminLayout()
