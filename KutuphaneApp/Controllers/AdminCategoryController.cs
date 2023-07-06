cc73c6a99a61f4176f1a06609ac24ce8a38c1855
Tarık Yüzgül <yuzgultarik@gmail.com>
2023-07-06 12:50:02 +0300

    Düzenleme

--- KutuphaneApp/Controllers/AdminCategoryController.cs
+++ KutuphaneApp/Controllers/AdminCategoryController.cs
@@ -1,11 +1,13 @@
-﻿using DataAccsessLayer.Abstract;
+﻿using BusinessLayer.ValidationRules;
+using DataAccsessLayer.Abstract;
+using EntityLayer.Concrete;
+using FluentValidation.Results;
 using Microsoft.AspNetCore.Mvc;
 
 namespace KutuphaneApp.Controllers
 {
     public class AdminCategoryController : Controller
     {
-
         private readonly ICategoryDal _categoryDal;
 
         public AdminCategoryController(ICategoryDal categoryDal)
@@ -18,6 +20,33 @@ namespace KutuphaneApp.Controllers
         {
             var categoryValues = _categoryDal.List();
             return View(categoryValues);
+        }
+
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddCategory(Category p)
+        {
+            CategoryValidator  categoryValidator = new CategoryValidator(); 
+            ValidationResult results = categoryValidator.Validate(p);
+            if (results.IsValid)
+            {
+                _categoryDal.Insert(p);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                foreach (var item in results.Errors)
+                {
+                    ModelState.AddModelError(item.PropertyName, item.ErrorMessage);
+                }
+            }
+
+            return View();
         }
 
         public IActionResult _AdminLayout()
